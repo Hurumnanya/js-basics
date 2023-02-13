f221e964c5a7df2cff27c5143d687fb4c024916d
Hurumnanya <105320491+Hurumnanya@users.noreply.github.com>
2023-02-13 21:08:34 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function multiply (firstnum, secondnum) {
+
+    return (firstnum * secondnum);
+
+}
+
+console.log(multiply(15, 4));
+
+
+
+let firstname = "Chihurumnanya";
+let lastname = "Ikevude";
+let track = "Backend developer";
+let skills = "Baking, lifestyle-coaching, web-development";
+let color = "White color";
+let independence = 1960;
+
+console.log("Hello ! ! ! my name is " + firstname + " "  + lastname + " I am a " + track + ",");
+
+console.log("Some of my skills include " + skills + " I love " + color + ",");
+
+console.log("My country got her independence in the year " + independence + ".");
+
+
